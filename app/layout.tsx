de6b5bb84713fe6b2b471a90298d5ea265771425
bryan <bry@payloro.com>
2024-10-08 12:05:46 +0800

    add thumbnail to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,29 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bryan's Portfolio",
   description: "Modern & Minimalist Portfolio",
+  metadataBase: new URL("https://bryanpalad.vercel.app"),
+  openGraph: {
+    title: "Bryan's Portfolio",
+    description: "A modern & minimalist web developer portfolio.",
+    url: "https://bryanpalad.vercel.app",
+    siteName: "Bryan's Portfolio",
+    images: [
+      {
+        url: "/BryanThumbnail.PNG",
+        width: 1200,
+        height: 630,
+        alt: "Bryan's Portfolio Thumbnail",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bryan's Portfolio",
+    description: "A modern & minimalist web developer portfolio.",
+    images: ["/BryanThumbnail.PNG"],
+  },
 };
 
 export default function RootLayout({
